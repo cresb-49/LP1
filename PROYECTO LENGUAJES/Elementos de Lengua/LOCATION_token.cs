3d749658ec57d03112f0ea53d65fe0d96f6846b2
cresb-49 <66991258+cresb-49@users.noreply.github.com>
2020-09-09 20:23:55 -0600

    token info

---
+++ PROYECTO LENGUAJES/Elementos de Lengua/LOCATION_token.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PROYECTO_LENGUAJES.Elementos_de_Lengua
+{
+    class LOCATION_token
+    {
+        private String cadena;
+        private int lineaUnicacion;
+        public LOCATION_token(String cadena, int linea)
+        {
+            this.cadena = cadena;
+            this.lineaUnicacion = linea;
+        }
+        public String getCadena()
+        {
+            return cadena;
+        }
+        public int getLineaUbicacion()
+        {
+            return lineaUnicacion;
+        }
+    }
+}
